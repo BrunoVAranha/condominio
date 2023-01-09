b5aa4856184fa7c44d86a06c8e583d5a57627a67
unknown <bruno.va.github@gmail.com>
2023-01-09 12:34:33 -0300

    remove lombok

--- src/main/java/com/proj/cond/domain/Condominio.java
+++ src/main/java/com/proj/cond/domain/Condominio.java
@@ -1,16 +1,9 @@
 package com.proj.cond.domain;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name="condominios")
-@Getter
-@Setter
-@NoArgsConstructor
 public class Condominio {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -30,5 +23,125 @@ public class Condominio {
     private String inscrMunCond;
     @OneToMany
     private List<Morador> moradores;
+
+    public Integer getCodCond() {
+        return codCond;
+    }
+
+    public void setCodCond(Integer codCond) {
+        this.codCond = codCond;
+    }
+
+    public String getNomeCond() {
+        return nomeCond;
+    }
+
+    public void setNomeCond(String nomeCond) {
+        this.nomeCond = nomeCond;
+    }
+
+    public String getNomeFant() {
+        return nomeFant;
+    }
+
+    public void setNomeFant(String nomeFant) {
+        this.nomeFant = nomeFant;
+    }
+
+    public String getEndCond() {
+        return endCond;
+    }
+
+    public void setEndCond(String endCond) {
+        this.endCond = endCond;
+    }
+
+    public Integer getCadCondNum() {
+        return cadCondNum;
+    }
+
+    public void setCadCondNum(Integer cadCondNum) {
+        this.cadCondNum = cadCondNum;
+    }
+
+    public String getBairroCond() {
+        return bairroCond;
+    }
+
+    public void setBairroCond(String bairroCond) {
+        this.bairroCond = bairroCond;
+    }
+
+    public String getCidCond() {
+        return cidCond;
+    }
+
+    public void setCidCond(String cidCond) {
+        this.cidCond = cidCond;
+    }
+
+    public String getUfCond() {
+        return ufCond;
+    }
+
+    public void setUfCond(String ufCond) {
+        this.ufCond = ufCond;
+    }
+
+    public String getCepCond() {
+        return cepCond;
+    }
+
+    public void setCepCond(String cepCond) {
+        this.cepCond = cepCond;
+    }
+
+    public String getEmailCond() {
+        return emailCond;
+    }
+
+    public void setEmailCond(String emailCond) {
+        this.emailCond = emailCond;
+    }
+
+    public String getTelFixoCond() {
+        return telFixoCond;
+    }
+
+    public void setTelFixoCond(String telFixoCond) {
+        this.telFixoCond = telFixoCond;
+    }
+
+    public String getCnpjCond() {
+        return cnpjCond;
+    }
+
+    public void setCnpjCond(String cnpjCond) {
+        this.cnpjCond = cnpjCond;
+    }
+
+    public String getInscrEstCond() {
+        return inscrEstCond;
+    }
+
+    public void setInscrEstCond(String inscrEstCond) {
+        this.inscrEstCond = inscrEstCond;
+    }
+
+    public String getInscrMunCond() {
+        return inscrMunCond;
+    }
+
+    public void setInscrMunCond(String inscrMunCond) {
+        this.inscrMunCond = inscrMunCond;
+    }
+
+    public List<Morador> getMoradores() {
+        return moradores;
+    }
+
+    public void setMoradores(List<Morador> moradores) {
+        this.moradores = moradores;
+    }
 
 }
--- src/main/java/com/proj/cond/domain/Morador.java
+++ src/main/java/com/proj/cond/domain/Morador.java
@@ -1,17 +1,11 @@
 package com.proj.cond.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 public class Morador {
 
     @Id
@@ -30,7 +24,6 @@ public class Morador {
     private String telFixMor;
     private String telCelMor;
     private String emailMorBoolean;
-
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "codCond")
@@ -38,4 +31,124 @@ public class Morador {
     //TODO VAGAS
 //    @OneToMany
 //    List<Vaga> vagas;
+
+    public Integer getCodMor() {
+        return codMor;
+    }
+
+    public void setCodMor(Integer codMor) {
+        this.codMor = codMor;
+    }
+
+    public String getNomeMor() {
+        return nomeMor;
+    }
+
+    public void setNomeMor(String nomeMor) {
+        this.nomeMor = nomeMor;
+    }
+
+    public Boolean getMorResp() {
+        return morResp;
+    }
+
+    public void setMorResp(Boolean morResp) {
+        this.morResp = morResp;
+    }
+
+    public Integer getBlocoMor() {
+        return blocoMor;
+    }
+
+    public void setBlocoMor(Integer blocoMor) {
+        this.blocoMor = blocoMor;
+    }
+
+    public Integer getAndarMor() {
+        return andarMor;
+    }
+
+    public void setAndarMor(Integer andarMor) {
+        this.andarMor = andarMor;
+    }
+
+    public Integer getAptoMor() {
+        return aptoMor;
+    }
+
+    public void setAptoMor(Integer aptoMor) {
+        this.aptoMor = aptoMor;
+    }
+
+    public LocalDate getDtIniMor() {
+        return dtIniMor;
+    }
+
+    public void setDtIniMor(LocalDate dtIniMor) {
+        this.dtIniMor = dtIniMor;
+    }
+
+    public LocalDate getDtSaidaMor() {
+        return dtSaidaMor;
+    }
+
+    public void setDtSaidaMor(LocalDate dtSaidaMor) {
+        this.dtSaidaMor = dtSaidaMor;
+    }
+
+    public LocalDate getDtNascMor() {
+        return dtNascMor;
+    }
+
+    public void setDtNascMor(LocalDate dtNascMor) {
+        this.dtNascMor = dtNascMor;
+    }
+
+    public String getCpfMor() {
+        return cpfMor;
+    }
+
+    public void setCpfMor(String cpfMor) {
+        this.cpfMor = cpfMor;
+    }
+
+    public String getRgMor() {
+        return rgMor;
+    }
+
+    public void setRgMor(String rgMor) {
+        this.rgMor = rgMor;
+    }
+
+    public String getTelFixMor() {
+        return telFixMor;
+    }
+
+    public void setTelFixMor(String telFixMor) {
+        this.telFixMor = telFixMor;
+    }
+
+    public String getTelCelMor() {
+        return telCelMor;
+    }
+
+    public void setTelCelMor(String telCelMor) {
+        this.telCelMor = telCelMor;
+    }
+
+    public String getEmailMorBoolean() {
+        return emailMorBoolean;
+    }
+
+    public void setEmailMorBoolean(String emailMorBoolean) {
+        this.emailMorBoolean = emailMorBoolean;
+    }
+
+    public Condominio getCondominio() {
+        return condominio;
+    }
+
+    public void setCondominio(Condominio condominio) {
+        this.condominio = condominio;
+    }
 }
